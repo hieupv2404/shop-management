d10b93c759fd2c2f8947eb0a709f3bc851d60343
lethanhdat215 <85091532+lethanhdat215@users.noreply.github.com>
2021-09-06 12:16:54 +0700

    fix add cart, edit cart, delete cart, checkout

---
+++ src/main/java/shoppingcart/service/OrderService.java
@@ -0,0 +1,16 @@
+package shoppingcart.service;
+
+import shoppingcart.DTO.CartDTO;
+import shoppingcart.DTO.Item;
+import shoppingcart.entity.Cart;
+import shoppingcart.entity.Order;
+import shoppingcart.entity.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public interface OrderService extends IGeneralService<Order>{
+    Integer saveOder(Order order);
+    Order makeOder(User user, Long totalPrice, HashMap<Integer, Item> carts);
+}
---
+++ src/main/java/shoppingcart/service/impl/OrderImpl.java
@@ -0,0 +1,89 @@
+package shoppingcart.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import shoppingcart.DTO.Item;
+import shoppingcart.entity.Cart;
+import shoppingcart.entity.Order;
+import shoppingcart.entity.OrderDetail;
+import shoppingcart.entity.User;
+import shoppingcart.repository.OrderDetailRepository;
+import shoppingcart.repository.OrderRepository;
+import shoppingcart.repository.UserRepository;
+import shoppingcart.service.CartSerice;
+import shoppingcart.service.OrderService;
+import shoppingcart.service.ProductService;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+
+public class OrderImpl implements OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+    @Autowired
+    private CartSerice cartSerice;
+    @Autowired
+    private ProductService productService;
+
+
+
+    @Override
+    public Iterable<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Optional<Order> findById(Integer id) {
+        return orderRepository.findById(id);
+    }
+
+    @Override
+    public Order save(Order order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public Integer saveOder(Order order) {
+        return orderRepository.save(order).getId();
+    }
+
+    @Override
+    public void remove(Integer id) {
+        orderRepository.deleteById(id);
+    }
+
+    @Override
+    public Order makeOder(User user, Long totalPrice, HashMap<Integer, Item> carts) {
+        userRepository.save(user);
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setFirstName(user.getFirstName());
+        order.setLastName(user.getLastName());
+        order.setAddress(user.getAddress());
+        order.setPhone(user.getPhone());
+        order.setTotalPrice(totalPrice);
+        order.setCreateDate(new java.util.Date());
+        orderRepository.save(order);
+
+        for(Map.Entry<Integer, Item> cart : carts.entrySet()) {
+            OrderDetail orderDetail = new OrderDetail();
+            Item item = cart.getValue();
+            orderDetail.setTotal(item.getProduct().getPrice()*item.getQuantity());
+            orderDetail.setQuantity(item.getQuantity());
+            orderDetail.setOder(orderRepository.findById(order.getId()).get());
+            orderDetail.setProduct(item.getProduct());
+            orderDetailRepository.save(orderDetail);
+        }
+        return order;
+    }
+}
